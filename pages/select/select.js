7e05124948b341500afb34af136e31530bba533f
guangxi.zhao <guangxi.developer@gmail.com>
2018-09-15 13:29:34 +0900

    getHistoryをコミット

--- pages/select/select.js
+++ pages/select/select.js
@@ -8,301 +8,25 @@ Page({
     select: '',
     startEndTrainFlg: true,
     getStationForNameResult: false,
-    //getStationForOtherResult: false,
-    //stationList: [],
-    getStationForOtherResult: true,
-    stationList: [{
-      stationNameCN: '函館',
-      stationNameJP: '函館JP',
-      stationNameRoman: 'hakodate',
-    },
-    {
-      stationNameCN: '五稜郭',
-      stationNameJP: '五稜郭JP',
-      stationNameRoman: 'goryoukaku'
-    }],
+    stationListForSelectName: [],
+    getStationForOtherResult: false,
+    stationList: [],
+    // TODO START
+     //getStationForOtherResult: true,
+     //stationList: [{
+      // stationNameCN: '函館',
+       //stationNameJP: '函館JP',
+       //stationNameRoman: 'hakodate',
+     //},
+     //{
+       //stationNameCN: '五稜郭',
+       //stationNameJP: '五稜郭JP',
+       //stationNameRoman: 'goryoukaku'
+     //}],
+    // TODO END
     placeholder: '请输入目的地',
     eventKbn: ['中文查询', '日文查询'],
-    eventKbn_index: 0,
-    zuijin: [{
-        name: '函館',
-        en: 'hakodate',
-      },
-      {
-        name: '五稜郭',
-        en: 'goryoukaku'
-      },
-      {
-        name: '桔梗',
-        en: 'kikyou'
-      },
-      {
-        name: '大中山',
-        en: 'oonakayama'
-      },
-      {
-        name: '七飯',
-        en: 'nanae'
-      },
-      {
-        name: '新函館北斗',
-        en: 'shinhakodatehokuto'
-      },
-      {
-        name: '仁山',
-        en: 'niyama'
-      },
-      {
-        name: '大沼',
-        en: 'oonuma'
-      },
-      {
-        name: '大沼公園',
-        en: 'oonumakouen'
-      },
-      {
-        name: '赤井川',
-        en: 'akaikawa'
-      },
-      {
-        name: '駒ケ岳',
-        en: 'komagatake'
-      },
-      {
-        name: '東山',
-        en: 'higashiyama'
-      },
-      {
-        name: '姫川',
-        en: 'himekawa'
-      },
-      {
-        name: '池田園',
-        en: 'ikedaen'
-      },
-      {
-        name: '流山温泉',
-        en: 'nagareyamaonsen'
-      },
-      {
-        name: '銚子口',
-        en: 'choushiguchi'
-      },
-      {
-        name: '鹿部',
-        en: 'shikabe'
-      },
-    ],
-
-    fujin: [{
-        name: '駒ケ岳',
-        en: 'komagatake'
-      },
-      {
-        name: '東山',
-        en: 'higashiyama'
-      },
-      {
-        name: '姫川',
-        en: 'himekawa'
-      },
-      {
-        name: '池田園',
-        en: 'ikedaen'
-      },
-      {
-        name: '流山温泉',
-        en: 'nagareyamaonsen'
-      },
-      {
-        name: '銚子口',
-        en: 'choushiguchi'
-      },
-      {
-        name: '鹿部',
-        en: 'shikabe'
-      },
-      {
-        name: '函館',
-        en: 'hakodate',
-      },
-      {
-        name: '五稜郭',
-        en: 'goryoukaku'
-      },
-      {
-        name: '桔梗',
-        en: 'kikyou'
-      },
-      {
-        name: '大中山',
-        en: 'oonakayama'
-      },
-      {
-        name: '七飯',
-        en: 'nanae'
-      },
-      {
-        name: '新函館北斗',
-        en: 'shinhakodatehokuto'
-      },
-      {
-        name: '仁山',
-        en: 'niyama'
-      },
-      {
-        name: '大沼',
-        en: 'oonuma'
-      },
-      {
-        name: '大沼公園',
-        en: 'oonumakouen'
-      },
-      {
-        name: '赤井川',
-        en: 'akaikawa'
-      },
-    ],
-
-    shoucang: [{
-        name: '姫川',
-        en: 'himekawa'
-      },
-      {
-        name: '池田園',
-        en: 'ikedaen'
-      },
-      {
-        name: '流山温泉',
-        en: 'nagareyamaonsen'
-      },
-      {
-        name: '銚子口',
-        en: 'choushiguchi'
-      },
-      {
-        name: '鹿部',
-        en: 'shikabe'
-      },
-      {
-        name: '函館',
-        en: 'hakodate',
-      },
-      {
-        name: '五稜郭',
-        en: 'goryoukaku'
-      },
-      {
-        name: '駒ケ岳',
-        en: 'komagatake'
-      },
-      {
-        name: '東山',
-        en: 'higashiyama'
-      },
-      {
-        name: '桔梗',
-        en: 'kikyou'
-      },
-      {
-        name: '大中山',
-        en: 'oonakayama'
-      },
-      {
-        name: '七飯',
-        en: 'nanae'
-      },
-      {
-        name: '新函館北斗',
-        en: 'shinhakodatehokuto'
-      },
-      {
-        name: '仁山',
-        en: 'niyama'
-      },
-      {
-        name: '大沼',
-        en: 'oonuma'
-      },
-      {
-        name: '大沼公園',
-        en: 'oonumakouen'
-      },
-      {
-        name: '赤井川',
-        en: 'akaikawa'
-      },
-    ],
-
-    shousuo: [{
-        name: '仁山',
-        en: 'niyama'
-      },
-      {
-        name: '大沼',
-        en: 'oonuma'
-      },
-      {
-        name: '大沼公園',
-        en: 'oonumakouen'
-      },
-      {
-        name: '姫川',
-        en: 'himekawa'
-      },
-      {
-        name: '池田園',
-        en: 'ikedaen'
-      },
-      {
-        name: '流山温泉',
-        en: 'nagareyamaonsen'
-      },
-      {
-        name: '銚子口',
-        en: 'choushiguchi'
-      },
-      {
-        name: '鹿部',
-        en: 'shikabe'
-      },
-      {
-        name: '函館',
-        en: 'hakodate',
-      },
-      {
-        name: '五稜郭',
-        en: 'goryoukaku'
-      },
-      {
-        name: '駒ケ岳',
-        en: 'komagatake'
-      },
-      {
-        name: '東山',
-        en: 'higashiyama'
-      },
-      {
-        name: '桔梗',
-        en: 'kikyou'
-      },
-      {
-        name: '大中山',
-        en: 'oonakayama'
-      },
-      {
-        name: '七飯',
-        en: 'nanae'
-      },
-      {
-        name: '新函館北斗',
-        en: 'shinhakodatehokuto'
-      },
-      {
-        name: '赤井川',
-        en: 'akaikawa'
-      },
-    ],
+    eventKbn_index: 0
   },
   onLoad: function(option) {
     console.log("onload" + option.placeholder);
@@ -317,10 +41,10 @@ Page({
       });
     }
     this.setData({
-      select: 'fujin',
-      testData1: this.data.fujin
+      select: 'zuijin'
     });
-    console.log("testData : " + this.data.testData);
+
+    this.getStationListForOther();
   },
 
   selectEvent: function(e) {
@@ -348,7 +72,7 @@ Page({
         if ("OK" == response.data.responseCode) {
           if (response.data.result.stationCount != 0) {
             self.setData({
-              testData: response.data.result.stationList,
+              stationListForSelectName: response.data.result.stationList,
               displFlg: true,
               getStationForNameResult: true
             });
@@ -373,21 +97,21 @@ Page({
     const that = this;
     if (index == '1') {
       that.setData({
-        select: 'fujin',
+        select: 'zuijin',
       });
     } else if (index == '2') {
       that.setData({
-        select: 'zuijin',
+        select: 'shoucang',
       })
     } else if (index == '3') {
       that.setData({
-        select: 'shoucang',
+        select: 'fujin',
       })
     }
     that.getStationListForOther();
   },
 
-//获取站点列表(附近/最近查询/收藏)
+  //获取站点列表(附近/最近查询/收藏)
   getStationListForOther() {
    const self = this;
    var url = null;
@@ -419,35 +143,25 @@ Page({
    wxRequest
      .postRequest(url, data)
      .then(response => {
-       if ("OK" == response.data.responseCode) {
+       if ("OK" == response.data.responseCode) { 
          if (response.data.result.stationCount != 0) {
            self.setData({
              stationList: response.data.result.stationList,
              getStationForOtherResult: true,
-             displFlg: ture
+             displFlg: false
            });
          } else {
            self.setData({
-             getStationForOtherResult: true,
-             stationList: [{
-               stationGroupCode: "1234567",
-               stationNameCN: '函館',
-               stationNameJP: '函館',
-               stationNameRoman: 'hakodate',
-             }],
+             getStationForOtherResult: false,
+             stationList: [],
              displFlg: ture
            });
          }
        } else {
          // Request NG
          self.setData({
-           getStationForOtherResult: true,
-           stationList: [{
-             stationGroupCode: "1234567",
-             stationNameCN: 'リクエストエラー',
-             stationNameJP: 'リクエストエラー',
-             stationNameRoman: 'requestError',
-           }],
+           getStationForOtherResult: false,
+           stationList: [],
            displFlg: ture
          });
        }
@@ -478,6 +192,74 @@ Page({
   showDelete: function (e) {
     console.log("======showDelete Event======");
     console.log(e);
+  },
+
+  //左滑刪除操作START
+  //手指刚放到屏幕触发
+  touchS: function (e) {
+    console.log("touchS" + e);
+    //判断是否只有一个触摸点
+    if (e.touches.length == 1) {
+      this.setData({
+        //记录触摸起始位置的X坐标
+        startX: e.touches[0].clientX
+      });
+    }
+  },
+  //触摸时触发，手指在屏幕上每移动一次，触发一次
+  touchM: function (e) {
+    console.log("touchM:" + e);
+    var that = this
+    if (e.touches.length == 1) {
+      //记录触摸点位置的X坐标
+      var moveX = e.touches[0].clientX;
+      //计算手指起始点的X坐标与当前触摸点的X坐标的差值
+      var disX = that.data.startX - moveX;
+      //delBtnWidth 为右侧按钮区域的宽度
+      var delBtnWidth = that.data.delBtnWidth;
+      var txtStyle = "";
+      if (disX == 0 || disX < 0) {//如果移动距离小于等于0，文本层位置不变
+        txtStyle = "left:0px";
+      } else if (disX > 0) {//移动距离大于0，文本层left值等于手指移动距离
+        txtStyle = "left:-" + disX + "px";
+        if (disX >= delBtnWidth) {
+          //控制手指移动距离最大值为删除按钮的宽度
+          txtStyle = "left:-" + delBtnWidth + "px";
+        }
+      }
+      //获取手指触摸的是哪一个item
+      var index = e.currentTarget.dataset.index;
+      var list = that.data.stationList;
+      //将拼接好的样式设置到当前item中
+      list[index].txtStyle = txtStyle;
+      //更新列表的状态
+      this.setData({
+        stationList: list
+      });
+    }
+  },
+  touchE: function (e) {
+    console.log("touchE" + e);
+    var that = this
+    if (e.changedTouches.length == 1) {
+      //手指移动结束后触摸点位置的X坐标
+      var endX = e.changedTouches[0].clientX;
+      //触摸开始与结束，手指移动的距离
+      var disX = that.data.startX - endX;
+      var delBtnWidth = that.data.delBtnWidth;
+      //如果距离小于删除按钮的1/2，不显示删除按钮
+      var txtStyle = disX > delBtnWidth / 2 ? "left:-" + delBtnWidth + "px" : "left:0px";
+      //获取手指触摸的是哪一项
+      var index = e.currentTarget.dataset.index;
+      var list = that.data.stationList;
+      list[index].txtStyle = txtStyle;
+      //更新列表的状态
+      that.setData({
+        stationList: list
+      });
+    }
   }
+  //左滑刪除操作END
+
 })
 
